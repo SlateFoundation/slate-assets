eab156b1cd5c9a1a32425c08922ccbe0d689f737
Nafis Bey <nafis@jarv.us>
2016-04-30 15:57:06 -0400

    Add Activity namespace to git tree config.

---
+++ php-config/Git.config.d/slate-assets.php
@@ -0,0 +1,12 @@
+<?php
+
+Git::$repositories['slate-assets'] = [
+    'remote' => 'https://github.com/SlateFoundation/slate-assets.git',
+    'originBranch' => 'master', //'builds/v1',
+    'workingBranch' => 'master', //builds/v1',
+    'trees' => [
+        'php-classes/Slate/Assets',
+        'php-classes/Emergence/Activity',
+        'php-config/Git.config.d/slate-assets.php'
+    ]
+];
