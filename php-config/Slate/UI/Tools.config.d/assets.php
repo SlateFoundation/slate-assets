e57e32aa757472ebbd83821aa52a12d88d3467d3
Chris Alfano <chris@jarv.us>
2016-08-27 21:12:07 +0000

    Update initial tools config

--- php-config/Slate/UI/Tools.config.d/assets.php
+++ php-config/Slate/UI/Tools.config.d/assets.php
@@ -1,16 +1,30 @@
 <?php
 
-Slate\UI\Tools::$tools['Assets'] = [
-    'Manager' => [
-        '_href' => '/manage#assets',
-        '_icon' => 'network'
-    ],
-    'My Assets' => [
-        '_href' => '/assets?set=mine',
-        '_icon' => 'binoculars'
-    ],
-    'Classroom Assets' => [
-        '_href' => '/assets?set=classroom',
-        '_icon' => 'telescope'
-    ]
+if (empty($GLOBALS['Session'])) {
+    return;
+}
+
+$assetsTools = [
+    '_icon' => 'network',
+#    'My Assets' => [
+#        '_href' => '/assets?set=mine',
+#        '_icon' => 'binoculars'
+#    ],
+#    'Classroom Assets' => [
+#        '_href' => '/assets?set=classroom',
+#        '_icon' => 'telescope'
+#    ]
 ];
+
+if ($GLOBALS['Session']->hasAccountLevel('Staff')) {
+    $assetsTools['Manage'] = '/manage#assets';
+
+    if ($GLOBALS['Session']->hasAccountLevel('Administrator')) {
+        $assetsTools['Exports'] = [
+            '_icon' => 'export',
+            'All Assets' => '/exports/assets.csv',
+        ];
+    }
+}
+
+Slate\UI\Tools::$tools['Assets'] = $assetsTools;
